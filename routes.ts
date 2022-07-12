2c44e48a99ead7dca19baacd9bbbcdc5f2ac7164
brennomeneses <brennocorps@gmail.com>
2022-07-12 18:41:51 -0300

    root route test

--- routes.ts
+++ routes.ts
@@ -1,12 +1,17 @@
+import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
 import statsControllers from "./controllers/statsControllers";
 
 const router = Router();
+const prisma = new PrismaClient();
 
-router.get("/", statsControllers.show);
+router.get("/", async (request, response)=>{
+    const allStats = await prisma.stats.findMany();
+    response.json(allStats);
+});
 router.post("/", statsControllers.create);
 
 // Test router
-router.get("/room", (req, res)=> res.json({status: "ok"}))
+router.get("/test", (req, res)=> res.json({status: "ok"}))
 
 export default router;
