7d6be5ea0a4ed44ef9c5f585d14b1eb7b77cb67e
brennomeneses <brennocorps@gmail.com>
2022-07-12 00:15:43 -0300

    Fix issues

--- routes.ts
+++ routes.ts
@@ -6,4 +6,7 @@ const router = Router();
 router.get("/room", statsControllers.show);
 router.post("/room", statsControllers.create);
 
+// Test router
+router.get("/", (req, res)=> res.json({status: "ok"}))
+
 export default router;
