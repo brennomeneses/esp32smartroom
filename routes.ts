13636338c6ede54286463cd21d30b23ad5b9e587
brennomeneses <brennocorps@gmail.com>
2022-07-12 19:17:25 -0300

    Fix timeout issue

--- routes.ts
+++ routes.ts
@@ -6,7 +6,4 @@ const router = Router();
 router.get("/", statsControllers.show);
 router.post("/", statsControllers.create);
 
-// Test router
-router.get("/room", (req, res)=> res.json({status: "ok"}))
-
 export default router;
