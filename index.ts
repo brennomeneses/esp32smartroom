a7e13577d7e36d771497083924e48370c23813b6
brennomeneses <brennocorps@gmail.com>
2022-07-13 23:09:13 -0300

    New CORS rules

--- index.ts
+++ index.ts
@@ -1,17 +1,12 @@
 import Express from 'express';
 import router from './routes';
+import cors from 'cors';
 
 const app = Express();
 
 app.use(Express.json());
 
-app.use(function(req, res, next){
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next();
-  });
+app.use(cors());
 
 app.use(router);
 
